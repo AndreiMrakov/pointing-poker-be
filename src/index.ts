00f12ec6350514122634b6c21dedc0f5020d5027
Andrei Kaptsiuh <kaptsiuhandrei@gmail.com>
2021-09-02 22:28:15 +0300

    feat: add logger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 require('dotenv').config();
 import express from 'express';
+const morgan = require('morgan');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(morgan(process.env.LOG_LEVEL));
 
 app.get('/', (req, res) => {
     res.send('Hello from Express');
