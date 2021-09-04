af5bedcb3363bec6e8ca6c1150f1337d4b5f686e
MakFil <makfil1177@gmail.com>
2021-09-04 16:43:43 +0300

    feat: import socket-io server to index.ts and add PORT_SOCKET and FE_PORT_SOCKET constants to .env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 require('dotenv').config();
 import express from 'express';
 import morgan from 'morgan';
+import './socket-io';
 
 const PORT = process.env.PORT || 3000;
 const LOG_LEVEL = process.env.LOG_LEVEL as string;
@@ -9,7 +10,7 @@ const app = express();
 app.use(morgan(LOG_LEVEL));
 
 app.get('/', (req, res) => {
-    res.send('Hello from Express');
+  res.send('Hello from Express');
 });
 
 app.listen(PORT, () => console.log(`Server starting at localhost:${PORT}`));
