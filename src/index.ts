ea08aee3473421d51ad22acee7e4c27231ffadc6
MakFil <makfil1177@gmail.com>
2021-09-05 18:58:36 +0300

    refactor: replace socket-server from socket-io to index and remove socket-io

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,43 @@
 require('dotenv').config();
 import express from 'express';
 import morgan from 'morgan';
-import './socket-io';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
-const PORT = process.env.PORT || 3000;
 const LOG_LEVEL = process.env.LOG_LEVEL as string;
 
+const PORT = process.env.PORT;
+
 const app = express();
-app.use(morgan(LOG_LEVEL));
+app.use(function (req, res, next) {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://www.differentServerDomain.fr https://www.differentServerDomain.fr'
+  );
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello from Express');
 });
 
-app.listen(PORT, () => console.log(`Server starting at localhost:${PORT}`));
+app.use(morgan(LOG_LEVEL));
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET'],
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.broadcast.emit('message', `A user ${socket.id} connected`);
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('message', `A user ${socket.id} disconnected`);
+  });
+});
+
+server.listen(PORT, () => console.log(`Socket-server started at localhost:${PORT}`));
