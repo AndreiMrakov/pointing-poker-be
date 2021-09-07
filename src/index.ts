f85d3f1dbc67d3891f6df31a1f6e740e94255ee1
MakFill <75532397+MakFill@users.noreply.github.com>
2021-09-07 17:15:01 +0300

    [PP-6] Add socket with one room and basic notifications (#4)
    * feat: add jest and config
    
    * feat: add test action
    
    * feat: add deploy action
    
    * fix: add newline in jest config
    
    * feat: add socket.io
    
    * feat: create socket.io server
    
    * feat: import socket-io server to index.ts and add PORT_SOCKET and FE_PORT_SOCKET constants to .env
    
    * docs: update readme with additional information
    
    * docs: update readme
    
    * docs: fix  readme
    
    * refactor: remove unused variables from .env
    
    * refactor: replace socket-server from socket-io to index and remove socket-io
    
    * fix: resolve conflicts (add devDependencies)
    
    * feat: add socket.io
    
    * feat: create socket.io server
    
    * feat: import socket-io server to index.ts and add PORT_SOCKET and FE_PORT_SOCKET constants to .env
    
    * refactor: remove unused variables from .env
    
    * refactor: replace socket-server from socket-io to index and remove socket-io
    
    * fix: resolve conflicts in yarn.lock
    
    * fix: remove unused feature
    
    * refactor: change PORT to 3000
    
    * refactor: change console log
    
    * feat: add path alias for src folder to jest config
    
    * feat: add path alias for src folder to jest config
    
    * refactor: rename src alias
    
    * fix: remove changes in tsconfig
    
    * refactor: remove .github
    
    Co-authored-by: Yury Bialou <Pradd.by@gmail.com>
    Co-authored-by: Andrei Kaptsiuh <kaptsiuhandrei@gmail.com>
    Co-authored-by: Andrei <76453952+andreikaptsiuh@users.noreply.github.com>
    Co-authored-by: Praddby <pradd.wow@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
 require('dotenv').config();
 import express from 'express';
 import morgan from 'morgan';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
-const PORT = process.env.PORT || 3000;
 const LOG_LEVEL = process.env.LOG_LEVEL as string;
 
+const PORT = process.env.PORT;
+
 const app = express();
-app.use(morgan(LOG_LEVEL));
 
 app.get('/', (req, res) => {
-    res.send('Hello from Express');
+  res.send('Hello from Express');
 });
 
-app.listen(PORT, () => console.log(`Server starting at localhost:${PORT}`));
+app.use(morgan(LOG_LEVEL));
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET'],
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.broadcast.emit('message', `A user ${socket.id} connected`);
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('message', `A user ${socket.id} disconnected`);
+  });
+});
+
+server.listen(PORT, () => console.log(`Server started at localhost:${PORT}`));
