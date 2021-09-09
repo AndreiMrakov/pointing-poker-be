8ebc179a6ced3dcfe926f26e5afdd978682f5d81
Yury Bialou <Pradd.by@gmail.com>
2021-09-09 17:25:48 +0300

    feat: add default type db

--- src/db.ts
+++ src/db.ts
@@ -5,7 +5,7 @@ export const sequlize =  new Sequelize(
   String(process.env.DB_USER),
   String(process.env.DB_PASSWORD),
   {
-    dialect: process.env.DB_CONNECT as Dialect,
+    dialect: (process.env.DB_CONNECT || "postgres") as Dialect,
     host: String(process.env.DB_HOST),
     port: Number(process.env.DB_PORT),
   }
