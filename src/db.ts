71401b76420a254478f82b3c0c3abff5825750fc
Yury Bialou <Pradd.by@gmail.com>
2021-09-10 10:09:55 +0300

    fix: fix connect to DB for Heroku

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,16 @@
 import { Dialect, Sequelize } from 'sequelize';
 
-export const sequlize =  new Sequelize(
-  String(process.env.DB_NAME),
-  String(process.env.DB_USER),
-  String(process.env.DB_PASSWORD),
-  {
-    dialect: (process.env.DB_CONNECT || "postgres") as Dialect,
-    host: String(process.env.DB_HOST),
-    port: Number(process.env.DB_PORT),
-  }
-);
+export const sequlize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  dialect: (process.env.DB_CONNECT || "postgres") as Dialect,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+});
