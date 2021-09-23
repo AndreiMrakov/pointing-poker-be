ba3c04e2b3907db42804d8ca1bfc20d9bff96bdf
Yury Bialou <Pradd.by@gmail.com>
2021-09-23 11:53:38 +0300

    feat: add services for start and finish

--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -1,7 +1,44 @@
+import { RoomState } from "../models/RoomState";
 import { Room } from "../models/Room";
 
+// states: ['beginning', 'progress', 'finished']
+
 class GameService {
-  
+  async setStartGame(id: string) {
+    const stateId = await this.getIdStateRoom("progress");
+    return await this.updateRoomGame(id, stateId);
+  }
+
+  async setFinishGame(id: string) {
+    const stateId = await this.getIdStateRoom("finished");
+    return await this.updateRoomGame(id, stateId);
+  }
+
+  async updateRoomGame(id: string, stateId: number) {
+    try {
+      const room = await Room.update(
+        { roomStateId: stateId },
+        {
+          where: { id },
+          returning: true,
+        }
+      );
+      return room;
+    } catch(e) {
+      return `Room id=${id} was not updated. ${e}.`;
+    }
+  }
+
+  async getIdStateRoom(title: string) {
+    try {
+      const state = await RoomState.findOne({
+        where: { title }
+      });
+      return state?.get().id;
+    } catch(e) {
+      return `Not found state for title: ${title}. ${e}.`;
+    }
+  }
 }
 
 export const gameService = new GameService();
--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,8 @@
 import { Server as HttpServer } from "http";
 import { Server, ServerOptions } from "socket.io";
+import { gameService } from "./services";
+import { ClientEvent } from "./utils/enums/ClientEvent";
+import { ServerEvent } from "./utils/enums/ServerEvent";
 
 export function createApplication(
   httpServer: HttpServer,
@@ -9,6 +12,19 @@ export function createApplication(
 
   io.on("connection", (socket) => {
     socket.broadcast.emit('message', `A user ${socket.id} connected`);
+
+    /* ---------- Events for Game ------------ */
+    socket.on(ClientEvent.GameStart, async (data: any) => {
+      const { id } = data;
+      const game = await gameService.setStartGame(id);
+      socket.emit(ServerEvent.GameStarted, game);
+    });
+    socket.on(ClientEvent.GameFinish, async (data: any) => {
+      const { id } = data;
+      const game = await gameService.setFinishGame(id);
+      socket.emit(ServerEvent.GameFinished, game);
+    });
+     /* ---------- End events for Game ------------ */
 
     socket.on('disconnect', () => {
       socket.broadcast.emit('message', `A user ${socket.id} disconnected`);
