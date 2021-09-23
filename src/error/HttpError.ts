2d67a7b626cc7b04b56d4cd922bc5b545c729896
Yury Bialou <Pradd.by@gmail.com>
2021-09-23 21:01:07 +0300

    feat: add error constructor

--- src/error/HttpError.ts
+++ src/error/HttpError.ts
@@ -8,6 +8,14 @@ export class HttpError extends Error {
   }
 
   static badRequest(message: string) {
+    return new HttpError(400, message);
+  }
+
+  static forbidden(message: string) {
+    return new HttpError(403, message);
+  }
+
+  static notFound(message: string) {
     return new HttpError(404, message);
   }
 
