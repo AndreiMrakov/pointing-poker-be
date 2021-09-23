a590c3851de7a7f291c43180d28fd392bb65ea3e
Yury Bialou <Pradd.by@gmail.com>
2021-09-23 08:41:06 +0300

    feat: add name events

---
+++ src/utils/enums/ClientEvent.ts
@@ -0,0 +1,6 @@
+export enum ClientEvent {
+  GameStart = 'game:start',
+  GameFinish = 'game:finish',
+  GameRestart = 'game:restart',
+  UserVoteTask = 'game:user-vote-task',
+}
---
+++ src/utils/enums/ServerEvent.ts
@@ -0,0 +1,6 @@
+export enum ServerEvent {
+  GameStarted = 'game:started',
+  GameFinished = 'game:finished',
+  GameRestarted = 'game:restarted',
+  UserVotedTask = 'game:user-voted-task',
+}
