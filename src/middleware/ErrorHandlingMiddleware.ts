7e0c3299cb514fce7fe0c00a21752406f220334c
Yury Bialou <Pradd.by@gmail.com>
2021-09-23 20:27:18 +0300

    refactor: add interface for error handly

--- src/middleware/ErrorHandlingMiddleware.ts
+++ src/middleware/ErrorHandlingMiddleware.ts
@@ -1,9 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpError } from '@/error';
 
-export const ErrorHandling = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof HttpError) {
-    return res.status(err.status).json({message: err.message});
+interface IErrorHandling {
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+}
+
+export const ErrorHandling = (data: IErrorHandling) => {
+  if (data.err instanceof HttpError) {
+    return data.res.status(data.err.status).json({message: data.err.message});
   }
-  return res.status(500).json({message: 'Internal Server Error'});
+  return data.res.status(500).json({message: 'Internal Server Error'});
 };
