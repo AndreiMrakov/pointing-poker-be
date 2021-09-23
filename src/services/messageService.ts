3bfbab63ab25025c54088c397d9dfadde9db9bd9
MakFil <makfil1177@gmail.com>
2021-09-23 17:26:36 +0300

    refactor: remove check incoming data

--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -1,4 +1,3 @@
-import { BaseError, HttpStatusCode } from '@/helpers';
 import { Message } from '@/db/models/Message';
 
 class MessageService {
@@ -11,13 +10,10 @@ class MessageService {
   }
 
   async createMessage(text: string, roomId: string, userId: number) {
-    if(!text || !roomId || !userId) {
-      return(new BaseError('Bad request', HttpStatusCode.BAD_REQUEST));
-    }
     const message = await Message.create({text, roomId, userId});
 
     return message;
-  }
+  };
 };
 
 export const messageService = new MessageService();
