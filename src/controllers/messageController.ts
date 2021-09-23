d65dcd251516031f2738e971414eef920c646639
MakFil <makfil1177@gmail.com>
2021-09-23 16:11:09 +0300

    refactor: messageController

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -1,16 +1,18 @@
-import { messageService } from '../services/messageService';
+import { BaseError, HttpStatusCode } from '@/helpers';
+import { messageService } from '@/services/messageService';
 import {Request, Response} from 'express';
+
 
 class MessageController {
   async getAllMessages(req: Request, res: Response) {
     try {
       const { roomId } = req.query;
 
-      const messages = await messageService.getAllMessages(roomId as string);
+      const messages = await messageService.getMessagesByRoomId(roomId as string);
 
       res.json(messages);
     } catch {
-      res.json('There are no messages in this room');
+      res.json(new BaseError('There are no messages in this room', HttpStatusCode.NOT_FOUND));
     }
   }
 };
