a18cc4529ab7b5309863c1aa87652182534d91a6
Yury Bialou <Pradd.by@gmail.com>
2021-09-26 21:52:11 +0300

    fix: fix conflict PR

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 import { Router } from "express";
 import { routerTask } from "./taskRouter";
-
-export const router = Router();
-
-router.use('/task', routerTask);
-=======
-import { Router } from 'express';
 import { messageRouter } from './messageRouter';
 
 export const router = Router();
 
-router.use('/messages', messageRouter)
->>>>>>> 50f15fb93605c0bccea3c0206674cc63282ef06f
+router.use('/task', routerTask);
+router.use('/messages', messageRouter);
