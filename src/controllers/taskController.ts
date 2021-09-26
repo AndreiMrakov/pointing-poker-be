c7d7ef4dba780830122e915de0d56b99a5db4615
Yury Bialou <Pradd.by@gmail.com>
2021-09-27 01:23:05 +0300

    feat: add try catch in task controller

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,14 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
-import { NotFound } from '@/error';
+import { BadRequest, NotFound } from '@/error';
 import { taskService } from '@/services';
 
 export class TaskController {
   static async getAllTasks(req: Request, res: Response, next: NextFunction) {
-    const { roomId } = req.query;
-    if (!roomId) {
-      return next(new NotFound('Not found room id'));
+    try {
+      const { roomId } = req.query;
+      if (!roomId) {
+        return next(new NotFound('Not found room id'));
+      }
+      const tasks = await taskService.getAllTasks(String(roomId));
+      res.json(tasks);
+    } catch {
+      return next(new BadRequest('Wrong room'));
     }
-    const tasks = await taskService.getAllTasks(String(roomId));
-    res.json(tasks);
   }
 }
