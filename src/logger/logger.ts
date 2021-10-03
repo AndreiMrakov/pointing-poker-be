8f55773e22c613257163dcebb22fe89310f79024
Andrei <kaptsiuhandrei@gmail.com>
2021-10-03 19:35:31 +0300

    feat: add logs in console

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -2,13 +2,21 @@ import { createLogger, format, transports } from 'winston';
 
 export const logger = createLogger({
   transports: [
-    new transports.File({
+    /* new transports.File({
       filename: 'LOG_LVL.log',
       format:format.combine(
           format.colorize(),
           format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
           format.align(),
           format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
-      )}),
+      )
+    }), */
+    new transports.Console(),
   ],
+  format:format.combine(
+    format.colorize(),
+    format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+    format.align(),
+    format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+)
 });
